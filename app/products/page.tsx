8e3c302cdd16aacbd112e5e27f094dc45299de29
Abdulbasitali3698 <43988292+Abdulbasitali3698@users.noreply.github.com>
2024-02-02 10:56:06 +0500

    .

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,6 +1,9 @@
+import Link from "next/link";
+
 export default function Products(){
     return(
         <div>
+            <Link href="/">Home</Link>
             <h1>Product List</h1>
             <h3>Product1</h3>
             <h3>Product2</h3>
